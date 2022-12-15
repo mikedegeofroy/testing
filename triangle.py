5d937c47adb17bd587456f196aaa9cb3a4412b6b
Mike de Geofroy <59496187+MikeDeGeofroy@users.noreply.github.com>
2022-12-15 14:06:15 +0300

    Added triangle file

--- triangle.py
+++ triangle.py
@@ -1,6 +1,17 @@
+<<<<<<< develop
 def area(a, b, c):
     return (a + b + c) / 2
 
 
 def perimeter(a, b, c):
     return a + b + c
+=======
+
+def area(a):
+    return a * a
+
+
+def perimeter(a):
+    return 4 * a
+
+>>>>>>> Added triangle file
